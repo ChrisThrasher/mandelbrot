ad66a3fc184aa4cb45a4f98c38d2af63721d1fb3
Chris Thrasher <chrisjthrasher@gmail.com>
2022-08-20 18:55:05 -0600

    Simplify FPS string generation

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -165,7 +165,7 @@ int main()
         window.display();
 
         auto text_builder = std::ostringstream();
-        text_builder << std::setw(4) << 1'000'000 / clock.restart().asMicroseconds() << " fps\n";
+        text_builder << std::setw(4) << int(1. / clock.restart().asSeconds()) << " fps\n";
         text_builder << std::setw(4) << max_iterations << " iters\n";
         text_builder << std::setprecision(1) << std::scientific << initial_extent / extent << '\n';
         text.setString(text_builder.str());
